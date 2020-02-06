890d0b63f5d58ba0663912bd7ed1996654755985
totobgycs <totobgycs@gmail.com>
2020-02-06 22:22:03 +0100

    Unit test and refactoring

--- numista/services/numista.py
+++ numista/services/numista.py
@@ -4,7 +4,10 @@ import requests
 
 class NumistaClient:
 
-    NUMISTA_URL = 'https://api.numista.com/api/v1/coins'
+    NUMISTA_URL = 'https://api.numista.com/api/v1/'
+    NUMISTA_SEARCH_COIN = 'coins'
+    NUMISTA_GET_COIN = 'coins/%s'
+    NUMISTA_GET_ISSUES = 'coins/%s/issues'
 
     ERRORS = {
         400	: 'Invalid parameter or missing mandatory parameter',
@@ -17,24 +20,21 @@ class NumistaClient:
         self.api_key = api_key
         self.HEADER = {'Numista-API-Key': api_key}
 
+    def __execute_function(self, function, params):
+        r = requests.get(self.NUMISTA_URL + function, headers=self.HEADER, params=params)
+        if r.status_code in self.ERRORS:
+            raise Exception(self.ERRORS[r.status_code])
+        return r.json()
+
     def search_coins(self, query, language='en', pages=1, per_page=50):
         payload = {'lang': language, 'q': query,
                    'page': pages, 'count': per_page}
-        r = requests.get(self.NUMISTA_URL, headers=self.HEADER, params=payload)
-        if r.status_code in self.ERRORS:
-            raise Exception(self.ERRORS[r.status_code])
-        return r.json()
+        return self.__execute_function(self.NUMISTA_SEARCH_COIN, payload)
 
     def get_coin(self, coin, language='en'):
         payload = {'lang': language}
-        r = requests.get(self.NUMISTA_URL + '/' + str(coin), headers=self.HEADER, params=payload)
-        if r.status_code in self.ERRORS:
-            raise Exception(self.ERRORS[r.status_code])
-        return r.json()
+        return self.__execute_function(self.NUMISTA_GET_COIN % coin, payload)
 
     def get_coin_issues(self, coin, language='en'):
         payload = {'lang': language}
-        r = requests.get(self.NUMISTA_URL + '/' + str(coin) + '/issues', headers=self.HEADER, params=payload)
-        if r.status_code in self.ERRORS:
-            raise Exception(self.ERRORS[r.status_code])
-        return r.json()
+        return self.__execute_function(self.NUMISTA_GET_ISSUES % coin, payload)
